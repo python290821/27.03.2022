f3e1fbe67d25c97d9b5c9a2d4062e98dfcb80da5
python290821 <90146835+python290821@users.noreply.github.com>
2022-03-27 21:18:57 +0300

    Add files via upload

---
+++ flask_demo/main.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template, url_for, request
+from flask import Flask, render_template, request, jsonify, make_response, session
+from flask_session import Session
+
+app = Flask(__name__)
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
+# localhost:5000/
+@app.route("/index.html")
+def home():
+    #
+    return render_template('index.html')
+
+app.run()
