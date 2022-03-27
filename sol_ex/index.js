92f9031377de84cb0276b540aa4c7ba5ddb6a964
python290821 <90146835+python290821@users.noreply.github.com>
2022-03-27 20:59:30 +0300

    Add files via upload

---
+++ sol_ex/index.js
@@ -0,0 +1,9 @@
+console.log('loading index.js code...')
+function btn_click() 
+{
+    // alert('clicked me')
+    x = parseInt(document.getElementById('x_txt').value)
+    y = parseInt(document.getElementById('y_txt').value)
+    document.getElementById('result').innerHTML = 
+       x + ' + ' + y + ' = ' + (x + y);
+}
